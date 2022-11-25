3a0364896f0f47a95330999de633db33ab62eba1
nicolanda <nicolaslandazabal@hotmail.com>
2022-11-25 12:11:11 -0500

    first commit

---
+++ src/api/apiSlice.js
@@ -0,0 +1,45 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const apiSlice = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:3000/",
+  }),
+  endpoints: (builder) => ({
+    getTasks: builder.query({
+      query: () => "/tasks",
+      providesTags: ["Task"],
+      transformResponse: (response) => response.sort((a, b) => b.id - a.id),
+    }),
+    createTask: builder.mutation({
+      query: (newTask) => ({
+        url: "/tasks",
+        method: "POST",
+        body: newTask,
+      }),
+      invalidatesTags: ["Task"],
+    }),
+    updateTask: builder.mutation({
+      query: (updateTask) => ({
+        url: `/tasks/${updateTask.id}`,
+        method: "PATCH",
+        body: updateTask,
+      }),
+      invalidatesTags: ["Task"],
+    }),
+    deleteTask: builder.mutation({
+      query: (id) => ({
+        url: `/tasks/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["Task"],
+    }),
+  }),
+});
+
+export const {
+  useGetTasksQuery,
+  useCreateTaskMutation,
+  useDeleteTaskMutation,
+  useUpdateTaskMutation,
+} = apiSlice;
---
+++ src/components/TaskForm.jsx
@@ -0,0 +1,26 @@
+import { useCreateTaskMutation } from '../api/apiSlice'
+
+
+export const TaskForm = () => {
+  const [createTask] = useCreateTaskMutation()
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+
+    const name  = e.target.elements.name.value.trim()
+    const description = e.target.elements.description.value.trim()
+    const completed = e.target.elements.completed.checked
+
+    createTask({name, description, completed})
+  }
+  
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" name='name' />
+      <input type="text" name='description' />
+      <input type="checkbox" name="completed" />
+      <button>
+        Add Task
+      </button>
+    </form>
+  )
+}
